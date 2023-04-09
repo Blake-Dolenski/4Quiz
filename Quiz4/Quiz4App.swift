491e9f6e03dc8d65dff435acef49ac5b799fd131
Blake <blakedolenski@gmail.com>
2023-04-09 18:21:30 -0500

    Initial Commit

---
+++ Quiz4/Quiz4App.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz4App.swift
+//  Quiz4
+//
+//  Created by Blake Dolenski on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Quiz4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
